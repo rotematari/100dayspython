88dc56006b5876475f35affc7621c7741c7ead1a
rotematari <rotem.atri@gmail.com>
2023-10-20 11:15:02 +0300

    day6

---
+++ day1.py
@@ -0,0 +1,6 @@
+print("Welcom to the band Name Generator")
+city = input("What's name of the city you grow up in ?")
+
+pet = input("what's your pet's name ?")
+
+print(f"your band name could be {city} {pet}")
